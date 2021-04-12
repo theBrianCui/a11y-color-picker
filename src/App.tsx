48c13f5ccc5fe4763cbc5925fcc200ba12e5e0b8
Brian Cui <brian.cui@live.com>
2021-04-12 15:21:11 -0700

    Implement hue rainbow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import * as React from 'react';
+import TinyColor from 'tinycolor2';
 import {ColorArea, IColorAreaProps} from './ColorArea';
+
+const RAINBOW: TinyColor.Instance[] = (() => {
+    const colors = [];
+    let r = 255;
+    let g = 0;
+    let b = 0;
+
+    for (g = 0; g <= 255; ++g) {
+        colors.push(TinyColor({ r,g,b }));
+    }
+    g = 255;
+
+    for (r = 255; r >= 0; r--) {
+        colors.push(TinyColor({ r,g,b }));
+    }
+    r = 0;
+
+    for (b = 0; b <= 255; ++b) {
+        colors.push(TinyColor({ r,g,b }));
+    }
+    b = 255;
+
+    for (g = 255; g >= 0; g--) {
+        colors.push(TinyColor({ r,g,b }));
+    }
+    g = 0;
+
+    for (r = 0; r <= 255; ++r) {
+        colors.push(TinyColor({ r,g,b }));
+    }
+    r = 255;
+
+    for (b = 255; b >= 0; b--) {
+        colors.push(TinyColor({ r,g,b }));
+    }
+    b = 0;
+
+    return colors;
+})();
 
 class App extends React.Component {
     render() {
-        const colors = ['red', 'green', 'blue'];
+        const colors = RAINBOW;
         const size = {
-            width: '50px',
-            height: '100px'
+            width: '0.5px',
+            height: '50px'
         };
 
         const colorAreas = colors.map(c => {
--- src/ColorArea.tsx
+++ src/ColorArea.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import * as TinyColor from 'tinycolor2';
 
 export interface IColorAreaProps {
-    color: string;
+    color: TinyColor.Instance;
     width: string;
     height: string;
 }
@@ -9,7 +10,7 @@ export interface IColorAreaProps {
 export class ColorArea extends React.Component<IColorAreaProps> {
     render() {
         const style = {
-            backgroundColor: this.props.color,
+            backgroundColor: this.props.color.toRgbString(),
             width: this.props.width,
             height: this.props.height
         }
